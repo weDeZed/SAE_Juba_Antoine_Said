83d3a96ca7c241afb79dd7c069d1be8d4e2d758f
XiangLuc <lxiang@iut.univ-paris8.fr>
2023-10-26 14:09:09 +0200

    Singleton pour la class Connect qui permet d'avoir une seule instance de Connect.

--- src/main/java/com/sae/sae_juba_antoine_said/Modele/BDD/Connect.java
+++ src/main/java/com/sae/sae_juba_antoine_said/Modele/BDD/Connect.java
@@ -1,11 +1,33 @@
 package com.sae.sae_juba_antoine_said.Modele.BDD;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connect {
     private static Connect getInstance = null;
-
+    private Connection connection;
     public Connect() {
+        try {
+            connection = DriverManager.getConnection(
+                    Params.dsn,
+                    Params.login,
+                    Params.password
+            );
+        }catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
 
+    public static Connect getConnectionInstance() {
+        if(getInstance == null) {
+            getInstance = new Connect();
+            return getInstance;
+        }
+        return getInstance;
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
