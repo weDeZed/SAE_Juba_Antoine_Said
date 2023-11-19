d7e4b99f9c75e442934ae87aea8def9c7086ff87
XiangLuc <lxiang@iut.univ-paris8.fr>
2023-11-19 19:11:04 +0100

    feedback

--- src/main/java/com/sae/sae_juba_antoine_said/controlleur/ControlleurFinDeJeu.java
+++ src/main/java/com/sae/sae_juba_antoine_said/controlleur/ControlleurFinDeJeu.java
@@ -1,21 +1,27 @@
 package com.sae.sae_juba_antoine_said.controlleur;
 
+import com.sae.sae_juba_antoine_said.bdd.SessionUtilisateur;
 import com.sae.sae_juba_antoine_said.vue.Music;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControlleurFinDeJeu {
+public class ControlleurFinDeJeu implements Initializable {
     Music music;
     @FXML
     Button relancerButton;
@@ -24,6 +30,21 @@ public class ControlleurFinDeJeu {
     Button quitButton;
     @FXML
     AnchorPane anchorPaneIdW;
+
+    @FXML
+    VBox feedbackForm;
+    @FXML
+    TextArea feedbackTextArea;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        updateVisibility();
+    }
+
+    private void updateVisibility() {
+        boolean estConnecte = SessionUtilisateur.estConnecte();
+        feedbackForm.setVisible(estConnecte);
+    }
 
     @FXML
     public void relancerJeu(ActionEvent event) throws IOException {
@@ -45,5 +66,9 @@ public class ControlleurFinDeJeu {
         System.exit(0);
         music.stopMusicVictoire();
         music.stopMusicDefaite();
+    }
+    public void envoyerFeedbackAction(ActionEvent event) {
+        String message = feedbackTextArea.getText();
+
     }
 }
