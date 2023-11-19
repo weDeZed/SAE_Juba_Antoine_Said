3f5f09e42b3ad35e3712bd357975c9a78c9a7a8f
XiangLuc <lxiang@iut.univ-paris8.fr>
2023-11-19 16:19:47 +0100

    correction des params

--- src/main/java/com/sae/sae_juba_antoine_said/modele/bdd/Params.java
+++ src/main/java/com/sae/sae_juba_antoine_said/modele/bdd/Params.java
@@ -2,13 +2,9 @@ package com.sae.sae_juba_antoine_said.modele.bdd;
 
 public class Params {
 
-    /*
+
     public static String dsn = "jdbc:postgresql://database-etudiants.iut.univ-paris8.fr/lxiang";
     public static String login = "lxiang";
-    public static String password = "Lucxiang0109?";
-     */
+    public static String password = "?";
 
-    public static String dsn = "jdbc:postgresql://localhost/luc";
-    public static String login = "luc";
-    public static String password = "12345";
 }
