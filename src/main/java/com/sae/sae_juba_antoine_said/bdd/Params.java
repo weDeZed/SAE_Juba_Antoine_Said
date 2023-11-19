db60d4660ce64d263675ad816b930e59ff4cf7cd
XiangLuc <lxiang@iut.univ-paris8.fr>
2023-11-19 21:26:54 +0100

    correction dans le code

--- src/main/java/com/sae/sae_juba_antoine_said/bdd/Params.java
+++ src/main/java/com/sae/sae_juba_antoine_said/bdd/Params.java
@@ -2,13 +2,7 @@ package com.sae.sae_juba_antoine_said.bdd;
 
 public class Params {
 
-    /*
     public static String dsn = "jdbc:postgresql://database-etudiants.iut.univ-paris8.fr/lxiang";
     public static String login = "lxiang";
     public static String password = "?";
-     */
-
-    public static String dsn = "jdbc:postgresql://localhost/luc";
-    public static String login = "luc";
-    public static String password = "12345";
 }
